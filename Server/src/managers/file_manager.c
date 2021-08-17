319e1ff90934d4c254c0dba0c2fbd43317f055cb
Abstractize <gababarca@hotmail.com>
2021-08-17 16:53:57 -0600

    [Fix] Verbage

--- Server/src/managers/file_manager.c
+++ Server/src/managers/file_manager.c
@@ -6,15 +6,14 @@ int count_word(const char *filename, const char *content, const char *word)
 
     int counter = 0;
 
-    const char *tmp_text, *tmp_text, *first_char, *text = content;
+    const char *holdcontent, *holdword, *wordtop, *text = content;
     while (*text != '\0')
     {
-        for (first_char = tmp_text = text, tmp_text = word;
-             *tmp_text != '\0' && *tmp_text == *tmp_text;
-             tmp_text++, tmp_text++)
+        for (wordtop = holdcontent = text, holdword = word;
+             *holdword != '\0' && *holdcontent == *holdword;
+             holdword++, holdcontent++)
             ;
-
-        if (!*tmp_text && (first_char == content || isspace(first_char[-1])) && (!*tmp_text || isspace(*tmp_text)))
+        if (!*holdword && (wordtop == content || isspace(wordtop[-1])) && (!*holdcontent || isspace(*holdcontent)))
         {
             counter++;
         }
