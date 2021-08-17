514e1871409aa6da0dc8db1bd6e186e6daf54607
Abstractize <gababarca@hotmail.com>
2021-08-17 13:18:14 -0600

    [Add] Doxygen

--- Client/src/Services/info_service.h
+++ Client/src/Services/info_service.h
@@ -1,4 +1,12 @@
 /**
- * Callback function for the web application on /helloworld url call
+ * Service to post info
+ */
+
+/**
+ * @brief posts document and word
+ * 
+ * @param curl instance of the curl
+ * @param url service URL to do the Post
+ * @return int CURLcode of the process 
  */
 int post_info(CURL *curl, char *url);
--- Server/src/API/file_controller.h
+++ Server/src/API/file_controller.h
@@ -1,5 +1,14 @@
 #include <ulfius.h>
 /**
+ * File and Word Controller
+ */
+
+/**
+ * @brief Post Word and File
  * 
+ * @param request posted info
+ * @param response info to client
+ * @param user_data authentication data
+ * @return int API Code
  */
 int post_file_info(const struct _u_request *request, struct _u_response *response, void *user_data);
