ad9f1f9f8f6a6b22653103c701f1fd941081a3df
Gabriel Abarca Aguilar <gababarca@hotmail.com>
2021-08-17 17:03:10 -0600

    Feature/#63 count (#5)
    * [Update] File Name
    
    * [Add] Count Logic
    
    * [Fix] Verbage

--- Server/src/Managers/file_manager.c
+++
@@ -1,6 +0,0 @@
-#include "../Data/file_repository.h"
-
-int count_word(const char *filename, const char *content, const char *word)
-{
-    create_file(filename, content);
-}
---
+++ Server/src/managers/file_manager.c
@@ -0,0 +1,24 @@
+#include "../data/file_repository.h"
+
+int count_word(const char *filename, const char *content, const char *word)
+{
+    create_file(filename, content);
+
+    int counter = 0;
+
+    const char *holdcontent, *holdword, *wordtop, *text = content;
+    while (*text != '\0')
+    {
+        for (wordtop = holdcontent = text, holdword = word;
+             *holdword != '\0' && *holdcontent == *holdword;
+             holdword++, holdcontent++)
+            ;
+        if (!*holdword && (wordtop == content || isspace(wordtop[-1])) && (!*holdcontent || isspace(*holdcontent)))
+        {
+            counter++;
+        }
+        *text++;
+    }
+
+    return counter;
+}
