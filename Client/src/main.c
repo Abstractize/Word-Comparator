86504c01f210f9a7dc1dc47c85298a0be344def5
Abstractize <gababarca@hotmail.com>
2021-08-20 21:12:34 -0600

    [Add] Client Loop

--- Client/src/main.c
+++ Client/src/main.c
@@ -1,23 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <string.h>
 #include <curl/curl.h>
 #include "services/info_service.h"
 
+#define MAX 255
+
 int main(int argc, char *argv[])
 {
+
     char *url = getenv("REST_URL");
     CURL *curl;
     CURLcode res;
 
-    printf("Connects to API URL: %s\n",url);
+    printf("Connects to API URL: %s\n", url);
 
     curl_global_init(CURL_GLOBAL_ALL);
 
     curl = curl_easy_init();
     if (curl)
     {
-        post_info(curl, url);
+        bool run = true;
+        while (run)
+        {
+            char filename[MAX];
+            printf("Please enter the filename: \n");
+            scanf("%s", filename);
+            printf("Chosen file is: %s\n\n", filename);
+
+            char word[MAX];
+            printf("Please enter the word to search: \n");
+            scanf("%s", word);
+            printf("Chosen word is: %s\n\n", word);
+
+            char *response = post_info(curl, url, filename, word);
+            printf("Number of times the word %s is in %s: %s\n\n", word, filename, response);
+        
+            char end;
+            printf("If you want to end the process please press 'f', otherwise press another key to continue. \n");
+            scanf("%c", &end);
+            if(end == 'f')
+            printf("Program Closed.\n");
+                run = false;
+        }
     }
     return 0;
 }
--- Client/src/services/info_service.c
+++ Client/src/services/info_service.c
@@ -3,34 +3,65 @@
 #include <string.h>
 #include <curl/curl.h>
 #include "service.h"
-
-CURLcode post_info(CURL *curl, char *url)
+struct memory
 {
+  char *response;
+  size_t size;
+};
+
+static size_t function_pt(void *data, size_t size, size_t nmemb, void *userp)
+{
+  size_t realsize = size * nmemb;
+  struct memory *mem = (struct memory *)userp;
+
+  char *ptr = realloc(mem->response, mem->size + realsize + 1);
+  if (ptr == NULL)
+    return 0; /* out of memory! */
+
+  mem->response = ptr;
+  memcpy(&(mem->response[mem->size]), data, realsize);
+  mem->size += realsize;
+  mem->response[mem->size] = 0;
+
+  return realsize;
+}
+
+char *post_info(CURL *curl, char *url, char *filename, char *word)
+{
+  char *path = "./data/";
+  char *file = (char *)malloc(1 + strlen(path) + strlen(filename));
+  strcpy(file, path);
+  strcat(file, filename);
+
   printf("Posting: %s\n", url);
   CURLcode res;
   curl_mime *form = NULL;
   curl_mimepart *field = NULL;
   struct curl_slist *headerlist = NULL;
   static const char buf[] = "Expect:";
-  
+
   form = curl_mime_init(curl);
 
   field = curl_mime_addpart(form);
-  curl_mime_name(field, "filename");
-  curl_mime_data(field, "hello.txt", CURL_ZERO_TERMINATED);
+  curl_mime_name(field, "file");
+  curl_mime_filedata(field, file);
 
   field = curl_mime_addpart(form);
-  curl_mime_name(field, "file");
-  curl_mime_data(field, "Hello World! Hello other World! Hello World Again!", CURL_ZERO_TERMINATED);
+  curl_mime_name(field, "filename");
+  curl_mime_data(field, filename, CURL_ZERO_TERMINATED);
 
   field = curl_mime_addpart(form);
   curl_mime_name(field, "word");
-  curl_mime_data(field, "Hello", CURL_ZERO_TERMINATED);
+  curl_mime_data(field, word, CURL_ZERO_TERMINATED);
 
   headerlist = curl_slist_append(headerlist, buf);
-  
+
   curl_easy_setopt(curl, CURLOPT_URL, url);
   curl_easy_setopt(curl, CURLOPT_MIMEPOST, form);
+
+  struct memory chunk = {0};
+  curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, function_pt);
+  curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *)&chunk);
 
   res = curl_easy_perform(curl);
 
@@ -39,5 +70,6 @@ CURLcode post_info(CURL *curl, char *url)
   curl_easy_cleanup(curl);
   curl_mime_free(form);
   curl_slist_free_all(headerlist);
-  return res;
+
+  return chunk.response;
 }
